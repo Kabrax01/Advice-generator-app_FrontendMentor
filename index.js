447fdf1f0910b6b20d93841ced83cf50db6aaf81
Kabrax01 <117947583+Kabrax01@users.noreply.github.com>
2023-08-17 18:22:04 +0200

    02

---
+++ index.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const adviceNumber = document.querySelector("#advice__number");
+const advice = document.querySelector("#advice");
+const dice = document.querySelector("#random__dice");
+const container = document.querySelector("#container");
+
+async function getAdvice() {
+  const response = await fetch("https://api.adviceslip.com/advice");
+  const adviceApi = await response.json();
+  console.log(adviceApi);
+
+  adviceNumber.textContent = `ADVICE #${adviceApi.slip.id}`;
+  advice.textContent = `"${adviceApi.slip.advice}"`;
+}
+
+getAdvice();
+
+dice.addEventListener("click", getAdvice);
+
+dice.addEventListener("click", (e) => {});
+
+window.addEventListener("load", (e) => {
+  container.classList.add("show");
+});
